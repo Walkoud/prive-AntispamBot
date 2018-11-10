7d36e8e64caed086e20c7ba968bdb640a4a321cf
Walkoud <38588921+Walkoud@users.noreply.github.com>
2018-11-10 20:15:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 
-client.login(config.token)
+client.login(process.env.TOKEN)
 
 client.on("ready", () => {
     console.log(`frais et dispo`);
