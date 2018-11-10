d857a250f01d64075ead9b9e19598ebadbbf72c3
Walkoud <38588921+Walkoud@users.noreply.github.com>
2018-11-10 21:12:01 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,91 +1,82 @@
-var config = require("./config.json")
-const fs = require("fs")
-const ms  = require("ms")
-const randomColor = require("randomcolor");
-const Discord = require("discord.js");
-const client = new Discord.Client();
+var discord = require('discord.js');
+var fs = require('fs');
+var randomColour = require('randomcolor'); // yes, the creator of this package does not speak the real english
+var Config = require('./config.json');
 
-
-client.login(process.env.TOKEN)
-
-client.on("ready", () => {
-    console.log(`frais et dispo`);
-});
-
-async function roleChange(){
-  try{
-    let guild = client.guilds.first()
-      config.roles.forEach(r =>{
-        var rColor = randomColor();
-        guild.roles.get(r).edit({color: rColor})
-      })
-  }catch(e){
-    if(e == "TypeError: Cannot read property 'roles' of undefined" || e == "TypeError: Cannot read property 'edit' of undefined") return
-    else{
-      console.log(e)
+class Bot {
+    constructor(){
+        this.servers = require('./servers.json');
+        this.discordClient = new discord.Client({sync: true});
+        
+        this.discordClient.on("ready", () => {this.initialize();});
+        
+        this.discordClient.on("message", (msg) => {this.processMessage(msg)});
+        
+        this.discordClient.login(Config.discord_token);
     }
-  }
+    
+    initialize() {
+        this.log("Connected to discord.");
+        
+        setInterval(() => {
+            this.randomizeRoleColors();
+        }, Config.randomize_delay*1000);
+    }
+    
+    processMessage(msg) {
+        if(msg.content.startsWith(">addrole")) {
+            for(var role of msg.mentions.roles.array()) {
+                msg.reply("Added " + role + " to list of rainbow roles.");
+                
+                this.addRainbowRole(msg.guild.id, role.id);
+            }
+        }
+    }
+    
+    randomizeRoleColors() {
+        for(var server in this.servers) {
+            var liveGuild = this.discordClient.guilds.get(server);
+            
+            if (!liveGuild) {
+                this.error("Guild with ID " + server+ " no longer exists or the bot has been removed from it.");
+                continue;
+            }
+            
+            for(var role of this.servers[server]) {
+                var liveRole = liveGuild.roles.get(role);
+                
+                liveRole.setColor(randomColour(), "Rainbowbot random role color randomizer.");
+            }
+        }
+    }
+    
+    addRainbowRole(guild, role) {
+        if(this.servers[guild] == undefined) {
+            this.servers[guild] = [];
+        }
+        
+        for(var existingRole of this.servers[guild]) {
+            if(existingRole == role) {
+                return "That role has already been added.";
+            }
+        }
+        
+        this.servers[guild].push(role);
+        this.saveServers();
+    } 
+    
+    saveServers() {
+        fs.writeFileSync("./servers.json", JSON.stringify(this.servers), "utf8");
+        this.log("Saved servers file.");
+    }
+    
+    log(message) {
+        console.log("\x1b[32mINFO\x1b[37m - \x1b[0m" + message);
+    }
+    
+    error(message) {
+        console.log("\x1b[31mERROR\x1b[37m - \x1b[0m" + message);
+    }
 }
 
-let delay = config.delay
-var main = setInterval(roleChange, ms(delay))
-
-
-client.on("message", async message => {
-    config = JSON.parse(fs.readFileSync("./config.json", "utf8"))
-    if(message.content.startsWith(config.prefix) && message.guild.member(message.author).hasPermission("ADMINISTRATOR")){
-      const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-      const command = args.shift().toLowerCase();
-      let nRole = message.mentions.roles.first()
-      if(!nRole) nRole = NaN
-
-      if(command == "roles"){
-
-        if(args[0] == "add"){
-          config.roles.push(nRole.id)
-          message.channel.send(`Role added`)
-        }
-        else if(args[0] == "list"){
-          let roles = new Array();
-          config.roles.forEach(r =>{
-            roles.push(message.guild.roles.get(r))
-          })
-          message.channel.send("Roles: "+roles)
-        }
-        else if(args[0] == "remove"){
-          const index = config.roles.indexOf(nRole.id);
-          config.roles.splice(index, 1);
-          message.channel.send(`Role Removed`)
-        }
-        else if(!args[0]){
-          message.channel.send(`Args: <add, list, remove> @role`)
-        }else{
-          message.channel.send(`Args: <add, list, remove> @role`)
-        }
-      }
-
-      if(command  == "delay"){
-        if(!args[0]){
-          message.channel.send("Delay: ' "+ config.delay +" '")
-        }else{
-        config.delay = args[0]
-        delay = args[0]
-        }
-      }
-      if(command == "prefix"){
-        if(!args[0]){
-          message.channel.send("Prefix: ' "+ config.prefix +" '")
-        }
-        else{
-          config.prefix = args[0]
-        }
-      }
-      if(command == "delay" || command == "roles"){
-        clearInterval(main)
-        main = setInterval(roleChange, ms(delay))
-      }
-      fs.writeFile("./config.json", JSON.stringify(config), (err) => {
-        if(err)return console.error(err)
-      });
-    }
-});
+var instance = new Bot();
